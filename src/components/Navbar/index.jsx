a1c8213b64e3887c89fad8e5d605ca075a509a0d
enrico brasil <enrico.brasil@prit.app>
2025-02-18 23:57:20 -0300

    first commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css'
+function Navbar(){
+    return (
+        <>
+            
+            <ul className="ContainerList">
+                <li>Produto</li>
+                <li>Sobre nos</li>
+                <li>Login</li>
+            </ul>
+            
+        </>
+    )
+}
+
+export default Navbar;
